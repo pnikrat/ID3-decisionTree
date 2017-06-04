0fb7c7a9d8856fc311c768cb1cd9d53061131799
pnikrat <sniffoza@gmail.com>
2017-06-04 14:41:02 +0200

    choose attribute for first division from tree root

--- case_runner.py
+++ case_runner.py
@@ -1,6 +1,5 @@
 import csv
-import numpy as np
-
+import math
 
 class SingleAlgorithmCase:
     """Handle single program run.
@@ -58,6 +57,60 @@ class TreeCreator:
     def __init__(self, training_labels, training_data):
         self.training_labels = training_labels
         self.training_data = training_data
+        self.main_set_entropy = None
+        self.tree_root = None
 
     def create_tree(self):
-        print(self.training_data, self.training_labels)
+        self.main_set_entropy = self.calculate_entropy(self.training_data)
+        if self.main_set_entropy == 0:
+            print("same decisions in all set")
+        else:
+            partition_attribute = self.choose_attribute_for_partition(self.training_data)
+            print(partition_attribute)
+            # start tree creation here
+
+    def calculate_entropy(self, cases):
+        entropy = 0.0
+        number_of_all_cases = len(cases)
+        case_decision_dict = self.create_dict_with_number_of_occurences(cases, -1)
+        for v in case_decision_dict.values():
+            entropy += (-(v/number_of_all_cases) * math.log((v/number_of_all_cases), 2))
+        return entropy
+
+    def create_dict_with_number_of_occurences(self, list_to_check, index):
+        dict_with_number_of_occurences = dict()
+        for x in list_to_check:
+            if x[index] not in dict_with_number_of_occurences.keys():
+                dict_with_number_of_occurences[x[index]] = 1
+            else:
+                dict_with_number_of_occurences[x[index]] += 1
+        return dict_with_number_of_occurences
+
+    def choose_attribute_for_partition(self, training_data):
+        total_attribute_entropy = dict()
+        for index, attribute in enumerate(self.training_labels[:-1]):
+            print(attribute, index)
+            attribute_specific_entropy = dict()
+            attribute_specific_division = self.create_dict_with_number_of_occurences(training_data, index)
+            print(attribute_specific_division)
+            for k,v in attribute_specific_division.items():
+                key_specific_cases = [x for x in training_data if x[index] == k]
+                attribute_specific_entropy[k] = self.calculate_entropy(key_specific_cases)
+            print(attribute_specific_entropy)
+            total_attribute_entropy[attribute] = self.calculate_weighed_entropy(attribute_specific_entropy,
+                                                                                attribute_specific_division)
+        total_attribute_info_gain = self.change_entropy_to_info_gain(total_attribute_entropy)
+        print(total_attribute_info_gain)
+        # return key with maximum value in dict
+        return max(total_attribute_info_gain, key=total_attribute_info_gain.get)
+
+    def calculate_weighed_entropy(self, entropies, weighs):
+        total_entropy = 0.0
+        number_of_cases = sum(weighs.values())
+        for k,v in weighs.items():
+            total_entropy += entropies[k] * (v/number_of_cases)
+        return total_entropy
+
+    def change_entropy_to_info_gain(self, entropies):
+        entropies_copy = {k: self.main_set_entropy - v for k,v in entropies.items()}
+        return entropies_copy
